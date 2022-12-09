8e3b181df6906a7d6a8175cb6d3abf947b34ee1b
bah abdourahmane <bahabdourahimane@gmail.com>
2022-12-09 23:52:53 +0100

    first build

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -15,17 +15,19 @@ const Navbar = () => {
                 <img src={logo} alt="" style={{ width: '130px' }}/>
             </a>
             <div>
-                <Navitems className={'hidden md:flex md:space-x-4'} />
+                <Navitems 
+                style={{
+                    left : toggle ? "0" : "-100%"
+                }}
+                handleClick={()=> setToggle(false)}
+                className={'flex flex-col md:flex-row transition-all absolute md:static top-0 h-screen md:h-auto w-full md:w-auto bg-[#0a192f] md:bg-none justify-center md:justify-start items-center md:flex md:space-x-4'} 
+                />
             </div>
             {/* mobile icon */}
             <div className='block md:hidden z-10' onClick={handleClick}>
                 {toggle ? <FaTimes size={25} className='cursor-pointer '/>:<FaBars size={25} className='cursor-pointer '/>}
             </div>
-            {/* Tablette and mobile menu */}
-            <div className={toggle ? 'absolute left-0 top-0 w-full h-screen bg-[#0a192f] flex justify-center items-center transition opacity-[1] ease-linear duration-1000 ': 'hidden opacity-0'}>
-                <Navitems handleClick={handleClick} className='text-center'/>
-            </div>
-
+            
             {/* Social icons */}
             <div className='hidden lg:flex fixed top-[35%] left-0 text-gray-300'>
                 <Sideicon/>
